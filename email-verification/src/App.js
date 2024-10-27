50ce500284294e340bd36644d7733387a1a92e5f
daniel komoe <daniel.komoe@epitech.eu>
2024-10-28 00:08:04 +0100

    [MOD] modif nodemailer services to gmail

--- email-verification/src/App.js
+++ email-verification/src/App.js
@@ -13,20 +13,20 @@ const App = () => {
       console.log('Token from URL:', tokenFromUrl);
 
       if (!tokenFromUrl) {
-        setMessage('No token provided');
+        setMessage('No token provided. Please check your email link.');
         setLoading(false);
-      } else {
-        try {
-          console.log('Verifying token:', tokenFromUrl);
-          const response = await axios.get(`https://ivoire-artisans-server.netlify.app/api/verify-email?token=${tokenFromUrl}`);
-          console.log('Verification response:', response.data);
-          setMessage(response.data.message);
-        } catch (error) {
-          console.error('Error verifying email:', error);
-          setMessage(error.response ? error.response.data.message : 'Error verifying email');
-        } finally {
-          setLoading(false);
-        }
+        return; // Ajoutez un return ici pour sortir de la fonction
+      }
+
+      try {
+        const response = await axios.get(`https://ivoire-artisans-server.netlify.app/api/verify-email?token=${tokenFromUrl}`);
+        console.log('Verification response:', response.data);
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error('Error verifying email:', error);
+        setMessage(error.response ? error.response.data.message : 'Error verifying email. Please try again.');
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -40,6 +40,7 @@ const App = () => {
       ) : (
         <div>
           <p>{message}</p>
+          {!loading && message && <a href="/">Go back to Home</a>} {/* Redirection vers la page d'accueil */}
         </div>
       )}
     </div>
